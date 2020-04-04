d0b8cdef8a813466da7cc35903de7ad1eabfe3be
Patil <ritepatil@deloitte.com>
2020-04-05 02:44:44 +0530

    Removed Controller class

--- src/main/java/com/estimate/app/EstimateMain.java
+++ src/main/java/com/estimate/app/EstimateMain.java
@@ -2,6 +2,9 @@ package com.estimate.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class EstimateMain {
@@ -9,8 +12,19 @@ public class EstimateMain {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(EstimateMain.class, args);
-		
-
 	}
+}
 
+@RestController
+class EstimateController {
+	
+	@RequestMapping(value ="/")
+	public String getData(){
+		return "Connected";
+	}
+	
+	@GetMapping(value ="/welcome")
+	public String welcomeMessage(){
+		return "Welcome to the Game";
+	}	
 }
